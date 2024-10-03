77d3ccc4918c65e90d7b7f7d8218d6faa8483815
breznovic <slymarkel@gmail.com>
2024-10-03 09:52:29 +0300

    feat: add store and main page draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import s from "./App.module.css";
+import { useAppDispatch, useAppSelector } from "./lib/hooks";
+import { useEffect } from "react";
+import { fetchNews } from "./store/features/news/newsSlice";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const news = useAppSelector((state) => state.news.news);
+
+  useEffect(() => {
+    dispatch(fetchNews());
+    const interval = setInterval(() => {
+      dispatch(fetchNews());
+    }, 60000);
+    return () => clearInterval(interval);
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Hacker News</h1>
+      <div>
+        {news.map((n) => (
+          <div key={n.id}>{n.title}</div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/store/features/news/newsSlice.ts
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+type NewsItem = {
+  id: number;
+  title: string;
+  author: string;
+  score: number;
+  url: string;
+  time: number;
+  comments: string[];
+};
+
+type NewsState = {
+  news: NewsItem[];
+  loading: boolean;
+};
+
+const initialState: NewsState = {
+  news: [],
+  loading: false,
+};
+
+export const fetchNews = createAsyncThunk("news/fetchNews", async () => {
+  const response = await axios.get(
+    "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+  );
+  const newsIds = response.data.slice(0, 100);
+  const newsPromises = newsIds.map((id: number) =>
+    axios.get(
+      `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+    )
+  );
+  const news = await Promise.all(newsPromises);
+  return news.map((item) => item.data);
+});
+
+const newsSlice = createSlice({
+  name: "news",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchNews.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchNews.fulfilled, (state, action) => {
+        state.news = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchNews.rejected, (state) => {
+        state.loading = false;
+      });
+  },
+});
+
+export default newsSlice.reducer;
